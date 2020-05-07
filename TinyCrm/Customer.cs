0d176f26b4eb492a1b3c110bad8674da23a4d93f
George pan <panagiotakopoulosgeorge@yahoo.gr>
2020-05-07 15:19:37 +0300

    Add project files.

---
+++ TinyCrm/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyCrm
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public DateTime Created { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string VatNumber { get; set; }
+        public decimal TotalGross { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime date { get; set; }
+
+        public Customer()
+        {
+        }
+
+        public bool IsValidVatNumber(string vatNumber)
+        {
+            return
+                !string.IsNullOrWhiteSpace(vatNumber) &&
+                vatNumber.Length == 9;
+        }
+    }
+}
---
+++ TinyCrm/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TinyCrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tinyCrmDbContext = new TinyCrmDbContext();
+
+            // Insert
+            var customer = new Customer()
+            {
+                Firstname = "Xaris",
+                Lastname = "Mpouras",
+                Email = "mpouras.gr"
+            };
+
+            tinyCrmDbContext.Add(customer);
+
+            var petrogiannos = new Customer()
+            {
+                //CustomerId = 155,
+                Firstname = "Dimitris",
+                Lastname = "Petrogiannos",
+                Email = "petrogiannos.gr"
+            };
+
+            tinyCrmDbContext.Add(petrogiannos);
+            tinyCrmDbContext.SaveChanges();
+
+            // Get data
+            var customer2 = tinyCrmDbContext
+                .Set<Customer>()
+                .Where(c => c.CustomerId == customer.CustomerId)
+                .SingleOrDefault();
+
+            // Update
+            customer2.VatNumber = "123456789";
+            tinyCrmDbContext.SaveChanges();
+
+            // Delete
+            tinyCrmDbContext.Remove(customer2);
+            tinyCrmDbContext.SaveChanges();
+        }
+    }
+}
